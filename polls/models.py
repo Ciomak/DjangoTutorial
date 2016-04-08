b73e8c52b6666f91466157897ab688c822647d8f
Kamil Ciomcia <kamil.ciomcia@gmail.com>
2016-04-08 09:49:15 +0200

    Utworzono model Question w aplikacji polls

--- polls/models.py
+++ polls/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_lenght=200)
+    pubDate = models.DateTimeField('pub_date')
