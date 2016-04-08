904b9a95e46ec6d15177e19f099a3709b16a5b2f
Kamil Ciomcia <kamil.ciomcia@gmail.com>
2016-04-08 09:52:00 +0200

    Utworzono model Choice w aplikacji polls

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
 class Question(models.Model):
     question = models.CharField(max_lenght=200)
     pubDate = models.DateTimeField('pub_date')
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice = models.CharField(mac_lenght=200)
+    vote = models.IntegerField()
